a0667b4f1bd0b894890c64125b8c4c73c8cb48a3
Philippe Traverse <philippe.traverse@gmail.com>
2017-01-26 08:01:00 +0000

    added fake db and moment

--- fake.js
+++ fake.js
@@ -1,6 +1,22 @@
 const faker = require('faker');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'toor',
+    database: 'fake'
+});
+connection.connect();
 
 var user = faker.internet.userName();
 console.log(user);
 var post = faker.lorem.sentence(); 
 console.log(post);
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
